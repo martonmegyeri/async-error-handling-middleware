43153df1c19b10ef4fb15c6c155067f1e81578ee
megyerimarton <megyerimarton@gmail.com>
2018-05-02 16:16:11 +0200

    Boom

---
+++ index.js
@@ -0,0 +1,21 @@
+const error = require('./middleware/error');
+const asyncMiddleware = require('./middleware/async');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+
+app.get('/', asyncMiddleware((req, res) => {
+  return Promise.reject(new Error('boom!'));
+}));
+
+app.use(error);
+
+
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
